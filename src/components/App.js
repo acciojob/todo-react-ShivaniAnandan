eb027b6ca3b197ec758304e0105aedf31b9048a0
Shivani Anandan <shivanianandhan28@gmail.com>
2023-07-25 22:33:32 +0530

    1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,50 @@
 
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [inputText, setInputText] = useState("");
+  const [listItems, setListItem] = useState([]);
+
+  function addListItem() {
+    setListItem((prevItem) => {
+      return [...prevItem, inputText];
+    });
+    setInputText("");
+  }
+
+  function hndleDeleteBtn(index) {
+    console.log(index);
+    let newlistItem = [...listItems];
+    newlistItem.splice(index, 1);
+    // const newListItem = listItems.splice(index, 1);
+    // console.log(newListItem)
+    setListItem(newlistItem);
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      {/* Do not remove the main div */}
 
-export default App
+      <h3>To-Do List</h3>
+      <div className="inputBtnDiv">
+        <input
+          type="text"
+          onChange={(e) => setInputText(e.target.value)}
+          value={inputText}
+        />
+        <button onClick={addListItem}>Add</button>
+      </div>
+      <ul>
+        {listItems.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button onClick={() => hndleDeleteBtn(index)}></button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
